850c6aa32a00d2bdcc4c4dcea533e8023a1c2227
vms <michail.vms@gmail.com>
2019-09-07 19:19:32 +0300

    fix mplex varint usage

--- src/protocols/mplex/mplex.lua
+++ src/protocols/mplex/mplex.lua
@@ -7,18 +7,20 @@ local bit = require ("bit32")
 local band = bit.band
 local rshift = bit.rshift
 
-MPLEX = Proto ("mplex", "MPLEX protocol")
+local MPLEX_UVARINT_MAX_SIZE = 10
 
-function MPLEX.dissector (buffer, pinfo, tree)
+mplex_proto = Proto ("mplex", "MPLEX protocol")
+
+function mplex_proto.dissector (buffer, pinfo, tree)
     pinfo.cols.protocol = "MPLEX"
     pinfo.cols.info = "MPLEX Body"
 
-    local header, headerSize = extractUvarint(buffer)
+    local header, headerSize = extractUvarint(buffer, MPLEX_UVARINT_MAX_SIZE)
     local headerTree = tree:add(buffer(0, headerSize), string.format("MPLEX header: uvarint decoded 0x%X", header))
     headerTree:add(buffer(0, headerSize), string.format("flags 0x%x", band(header, 0x7)))
     headerTree:add(buffer(0, headerSize), string.format("stream id 0x%x", rshift(header, 3)))
 
-    local len, lenSize = extractUvarint(buffer(headerSize, buffer:len() - headerSize))
+    local len, lenSize = extractUvarint(buffer(headerSize, buffer:len() - headerSize), MPLEX_UVARINT_MAX_SIZE)
     local lenTree = tree:add(buffer(headerSize, lenSize), string.format("MPLEX len: uvarint decoded 0x%X", len))
 
 end
